1ac5c8f92e226dc67e3f1be3d446485bcfd9032b
ritam <38151929+rit-am@users.noreply.github.com>
2018-06-30 20:46:56 +0530

    Update ssps.ps1

--- ssps.ps1
+++ ssps.ps1
@@ -1,4 +1,4 @@
-﻿#usage     > PS C:\Users\ritam\Downloads\scripts_ps> .\ssps.ps1 -file "C:\Users\ritam\Downloads\data\xls\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
+﻿#usage     > PS ..\PS1\scripts_ps> .\ssps.ps1 -file "..\xls\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
 #output    > 0000000123
 param(
         [string]$file,
@@ -8,24 +8,4 @@ param(
         )
     $objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
     Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
-    $objExcel.quit();  
-
-
-<#
-GET-XLS-Cell-value -file ".\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
-
-Function GET-XLS-Cell-value()
-    {
-    param(
-        [string]$file,
-        [string]$sheetname,
-        [int]$row,
-        [int]$col
-        )
-    $objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
-    Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
-    $objExcel.quit();  
-    }
-#>
-
-         
+    $objExcel.quit();
