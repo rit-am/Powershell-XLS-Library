550b7037bb7ed690f117f0aeb46fb48aeb2b4650
ritam <38151929+rit-am@users.noreply.github.com>
2018-06-30 19:06:13 +0530

    Add files via upload

---
+++ ssps.ps1
@@ -0,0 +1,14 @@
+﻿GET-XLS-Cell-value -file ".\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
+
+Function GET-XLS-Cell-value()
+    {
+    param(
+        [string]$file,
+        [string]$sheetname,
+        [int]$row,
+        [int]$col
+        )
+    $objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
+    Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
+    $objExcel.quit();  
+    }         
