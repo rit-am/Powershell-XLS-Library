083872e56ab58b4fdd3edafa5030769e58f9eb75
ritam <38151929+rit-am@users.noreply.github.com>
2018-06-30 20:10:16 +0530

    enables command line

--- ssps.ps1
+++ ssps.ps1
@@ -1,4 +1,18 @@
-﻿GET-XLS-Cell-value -file ".\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
+﻿#usage     > PS C:\Users\ritam\Downloads\scripts_ps> .\ssps.ps1 -file "C:\Users\ritam\Downloads\data\xls\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
+#output    > 0000000123
+param(
+        [string]$file,
+        [string]$sheetname,
+        [int]$row,
+        [int]$col
+        )
+    $objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
+    Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
+    $objExcel.quit();  
+
+
+<#
+GET-XLS-Cell-value -file ".\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
 
 Function GET-XLS-Cell-value()
     {
@@ -11,4 +25,7 @@ Function GET-XLS-Cell-value()
     $objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
     Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
     $objExcel.quit();  
-    }         
+    }
+#>
+
+         
