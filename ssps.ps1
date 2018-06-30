43327fd0dda2aac3095820cb6fdbfb2b6122a99e
ritam <38151929+rit-am@users.noreply.github.com>
2018-06-30 20:50:45 +0530

    Add files via upload

--- ssps.ps1
+++ ssps.ps1
@@ -1,11 +1,6 @@
-﻿#usage     > PS ..\PS1\scripts_ps> .\ssps.ps1 -file "..\xls\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
+﻿#usage     > PS ..> .\PS1\ssps.ps1 -file ".\xls\cstmxl.xlsx" -sheet "CustomSheet" -row 2 -col 9
 #output    > 0000000123
-param(
-        [string]$file,
-        [string]$sheetname,
-        [int]$row,
-        [int]$col
-        )
-    $objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
-    Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
-    $objExcel.quit();
+param([string]$file,[string]$sheetname,[int]$row,[int]$col)
+$objExcel=New-Object -ComObject Excel.Application;$objExcel.Visible=$false;$objExcel.DisplayAlerts=$false
+Write-Host($objExcel.Workbooks.Open($file).Sheets.Item($sheetname).Cells.Item($row,$col).text)
+$objExcel.quit();
